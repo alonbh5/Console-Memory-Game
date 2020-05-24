ebe1b21979ecb9d99302ca25fad87087f0a134dd
Alon Ben Harosh <alonbh5@gmail.com>
2020-05-24 13:04:53 +0300

    Program class

--- B20_Ex02/B20_Ex02/Program.cs
+++ B20_Ex02/B20_Ex02/Program.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ex02.ConsoleUtils;
 
 namespace B20_Ex02
 {
     class Program
     {
+        static void Main()
+        {
+            Board try1 = new Board(4, 6);
+
+
+            Console.WriteLine(try1.ToStringBuilder());
+        }
+
+
     }
 }
