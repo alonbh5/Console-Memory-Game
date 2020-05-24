076cd0c447a89f264011a33d183f9fb332c7a95c
Alon Ben Harosh <alonbh5@gmail.com>
2020-05-24 13:04:38 +0300

    TIle class

---
+++ B20_Ex02/B20_Ex02/Tile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B20_Ex02
+{
+    struct Tile
+    {
+        private int m_Value;
+        private bool m_Expose;
+
+        internal Tile(int Value)
+        {
+            m_Value = Value;
+            m_Expose = false;
+        }
+
+        public int Value
+        {
+            get { return m_Value; }
+            set { m_Value = value; }
+        }
+
+        internal bool Expose
+        {
+            get { return m_Expose; }
+            set { m_Expose = value; }
+        }
+
+    }
+}
