5fa5cea38ba1643533f045c8e96b206aea422251
Alon Ben Harosh <alonbh5@gmail.com>
2020-05-24 13:06:16 +0300

    Player CLass

---
+++ B20_Ex02/B20_Ex02/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B20_Ex02
+{
+    class Player
+    {
+        string m_Name;
+        bool m_Pc;
+        int m_Pairs;
+
+        internal Player(string name, bool pc)
+        {
+            m_Name = name;
+            m_Pc = pc;
+        }
+
+        public string Name
+        {
+            get { return m_Name; }
+        }
+    }
+}
