4c6aef2d1ace1136d38d01d4479dbf77ed8ff19d
Alon Ben Harosh <alonbh5@gmail.com>
2020-05-24 15:54:36 +0300

    AI

--- B20_Ex02/B20_Ex02/AI.cs
+++ B20_Ex02/B20_Ex02/AI.cs
@@ -11,6 +11,7 @@ namespace B20_Ex02
         int row;
         int col;
         int value;
+        bool partner;
     }
 
 
@@ -18,31 +19,41 @@ namespace B20_Ex02
     {
         Turns[] m_turnMem = new Turns[6];
         int m_turns = 0;
-
-        internal AI()
-        {
-            m_turnMem 
-        }       
-    }
-    
-    
-
+        int m_pairsMem = 0;
+        int m_indexToAdd = -1;
+                         
 
         public void PlayTurn(ref int row, ref int col, Board gameboard)
         {
             m_turns++;
 
-            if (m_turns%3==0)
-            {
+            if (m_turns%3==0 && m_pairsMem > 0)
+            { // Play smart every 3 turns and if there is a pair in memory
                 smartchoice():
             }
             else
             {
                 randomchoice();
             }
-
-
         }
+
+        private void smartchoice(ref int row1, ref int col1, Board gameboard)
+        {
+            foreach (Turns i in m_turnMem)
+            {
+            }
+        }
+
+        internal void updateMemory(int i_Row, int i_Col)
+        {
+            m_indexToAdd++;
+            if (m_indexToAdd>m_turnMem.Length)
+            {
+                m_i
+            }
+        }
+
+
 
     }
 }
